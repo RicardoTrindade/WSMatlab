107840d908b0411a52e0981d29c5705049e142d5
RicardoTrindade <ricardo.trindade743@gmail.com>
2017-10-11 23:11:47 +0100

    removes not needed for loop

--- vector2String.m
+++ vector2String.m
@@ -1,12 +1,8 @@
 function [ S ] = vector2String( M )
-%MATRIX2STRING transform a matrix M , where M_{ij} \in {0,1,2,3} to array of strings {a,c,g,t}
 S = '';
 s = 'acgt';
-    for i = 1:size(M,1)
-        x = '';
-        for j=1:size(M,2)
-         x = [s(M(i,j) + 1), x];
-        end
+    for i = 1:length(M)
+    	x = s(M(i) + 1);
         S = [x; S];
     end
 end
